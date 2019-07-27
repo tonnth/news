829ffa7310c0515d3b4dc029c160265871c417a9
Tôn Nguyễn <hoangton1996@gmail.com>
2019-07-27 18:27:24 +0700

    Updates

---
+++ precache-manifest.cad83da714bbbe941aaa1ce196d24f2d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "476e7bbea3adf20c92a0758bf7bf727e",
+    "url": "/news/index.html"
+  },
+  {
+    "revision": "06adf6536655e88d4802",
+    "url": "/news/static/css/main.c58d0fb9.chunk.css"
+  },
+  {
+    "revision": "69da0d07073488974cc7",
+    "url": "/news/static/js/2.323e4b27.chunk.js"
+  },
+  {
+    "revision": "06adf6536655e88d4802",
+    "url": "/news/static/js/main.06bc7eaa.chunk.js"
+  },
+  {
+    "revision": "b6263ef5dad59d00f0a9",
+    "url": "/news/static/js/runtime~main.726e687c.js"
+  }
+]);
